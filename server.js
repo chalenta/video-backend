c0f6a07585dbab5467d5e836944e88542d8d3e9d
Paschal Ezehi <chalenta@gmail.com>
2026-05-06 15:48:36 +0100

    Add files via upload
    Initial backend upload

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+
+    socket.on("join-room", ({ roomId, userId }) => {
+        socket.join(roomId);
+        socket.userId = userId;
+
+        const users = [...io.sockets.adapter.rooms.get(roomId) || []]
+            .map(id => io.sockets.sockets.get(id)?.userId)
+            .filter(Boolean);
+
+        socket.emit("existing-users", users);
+
+        socket.to(roomId).emit("user-connected", userId);
+
+        socket.on("offer", data => {
+            io.to(getSocketId(data.target)).emit("offer", {
+                sender: userId,
+                sdp: data.sdp
+            });
+        });
+
+        socket.on("answer", data => {
+            io.to(getSocketId(data.target)).emit("answer", {
+                sender: userId,
+                sdp: data.sdp
+            });
+        });
+
+        socket.on("ice-candidate", data => {
+            io.to(getSocketId(data.target)).emit("ice-candidate", {
+                sender: userId,
+                candidate: data.candidate
+            });
+        });
+
+        socket.on("disconnect", () => {
+            socket.to(roomId).emit("user-disconnected", userId);
+        });
+    });
+});
+
+function getSocketId(userId) {
+    for (let [id, socket] of io.of("/").sockets) {
+        if (socket.userId === userId) return id;
+    }
+}
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
